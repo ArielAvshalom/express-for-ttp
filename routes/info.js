9eb7fc924f70ceb5a3ea3d2f85113dc8eb291453
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-01-19 09:44:49 -0500

    Added router.route

--- routes/info.js
+++ routes/info.js
@@ -24,6 +24,23 @@ router.get("/:id", (req, res) => {
     res.send(`Here is a piece of information with this ID: ${req.params.id}`)
 })
 
+//what happens if we move /info/new here???
+//Express works top down...
+
+//do this for all other necessary commands: like delete or put. You can chain all of these together with .route.
+
+
+
+router.
+    route("/:id")
+    .get((req, res) => {
+        res.send(`get info with id... ${req.params.id}`)
+    })
+    .put((req, res) => {
+        res.send(`now updating info with id... ${req.params.id}`)
+    })//you can add anything else you want here.
+
+
 
 
 module.exports = router
