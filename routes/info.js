c78b6a9367dcbe1f543cbee7721f4894ce76a8ad
ArielAvshalom <42783606+ArielAvshalom@users.noreply.github.com>
2022-01-19 10:48:08 -0500

    Update info.js

--- routes/info.js
+++ routes/info.js
@@ -3,11 +3,11 @@ const router = express.Router() //capital letter
 //this is independent from the main router sort of like its own compartmentalized piece.
 
 router.get('/', (req, res) => {
-    console.log('information router')
+    console.log('Information Router')
     res.send("Information")
 })
 
-router.get("/new", (req, res) => {
+router.get("/new/1", (req, res) => {
     res.send("New information")
 })
 
@@ -20,9 +20,19 @@ router.post("/", (req, res) => {
     res.send("Create Information")
 })
 
-router.get("/:id", (req, res) => {
-    res.send(`Here is a piece of information with this ID: ${req.params.id}`)
-})
+
+
+// router.get("/:id", (req, res) => {
+//     res.send(`Here is a piece of information with this ID: ${req.params.id}`)
+// })
+//
+// router.post("/:id", (req, res) => {
+//     res.send(`Here is a piece of information that we posted with this ID: ${req.params.id}`)
+// })
+//
+// router.delete("/:id", (req, res) => {
+//     res.send(`Here is a piece of information that we deleted with this ID: ${req.params.id}`)
+// })
 
 //what happens if we move /info/new here???
 //Express works top down...
@@ -34,13 +44,28 @@ router.get("/:id", (req, res) => {
 router.
     route("/:id")
     .get((req, res) => {
+        console.log(req.titleName)
         res.send(`get info with id... ${req.params.id}`)
     })
     .put((req, res) => {
         res.send(`now updating info with id... ${req.params.id}`)
-    })//you can add anything else you want here.
+    }).delete(
+    (req, res) => {
+        res.send(`Here is a piece of information that we deleted with this ID: ${req.params.id}`)
+    })//you can add anything else you want here (provided the method exists).
 
+//what do we do with all of this?
 
+//let's add some middleware:
+//
+// const infoArticles = [{title : "aphrodisiacs"}, {title : "fungi don't do what I want?"}, {title : "communism breaks python... :( you can't force all classes to be equal"}]
+//
+//let's put that into a very simple piece of middleware called param
+
+router.param('id', (req, res, next, id) => {
+    req.titleName = infoArticles[id]
+    next() //let's see what happens if we don't include next() here...
+})
 
 
 module.exports = router
